3b5f74805532c4c89090f0079f56ba8738fd90d4
EuGeN-PYTHON <78923547+EuGeN-PYTHON@users.noreply.github.com>
2022-12-03 10:25:06 +0300

    update db

--- ProductPlatform/settings.py
+++ ProductPlatform/settings.py
@@ -81,20 +81,20 @@ WSGI_APPLICATION = 'ProductPlatform.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'mydatabase',
         # 'NAME': 'storefood',
-        'USER': 'root',
+        # 'USER': 'root',
         # 'USER': 'moderator',
         # 'PASSWORD': '27June2010',
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    }
+        # 'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        # 'PORT': '3306',
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'dc18nq7jd8hffp',
